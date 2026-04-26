e0a31fe9edddbe32750aaef8ef5ca04bb3f7f6bc
Sayrarm <minsecond@mail.ru>
2026-04-26 21:41:43 +0300

    feat: add design for spacepedia

--- src/pages/Spacepedia.jsx
+++ src/pages/Spacepedia.jsx
@@ -1,10 +1,11 @@
 import {Navigate, useParams} from 'react-router-dom';
 import SpacepediaNavigation from "../components/SpacepediaNavigation.jsx";
-import { spacepediaData } from '../data/spacepedia-data.js';
+import {spacepediaData} from '../data/spacepedia-data.js';
 import styles from "./Spacepedia.module.css";
+import {Collapse} from 'antd';
 
 function Spacepedia() {
-    const { navigation } = useParams(); // получаем "guidance", "message" и т.д.
+    const {navigation} = useParams(); // получаем "guidance", "message" и т.д.
 
     // Фильтруем статьи по navigation
     const articles = spacepediaData.filter(
@@ -13,14 +14,14 @@ function Spacepedia() {
 
     // Если нет параметра — редиректим на guidance
     if (!navigation) {
-        return <Navigate to="/spacepedia/guidance" replace />;
+        return <Navigate to="/spacepedia/guidance" replace/>;
     }
 
     // Если нет выбранной навигации или нет статей
     if (!navigation || articles.length === 0) {
         return (
             <>
-                <SpacepediaNavigation />
+                <SpacepediaNavigation/>
                 <div className={styles.content}>
                     <p>No articles found ¯\_(ツ)_/¯</p>
                 </div>
@@ -30,15 +31,18 @@ function Spacepedia() {
 
     return (
         <>
-            <SpacepediaNavigation />
-            <div className={styles.content}>
-                {articles.map(article => (
-                    <article key={article.id}>
-                        <h3>{article.title}</h3>
-                        <div dangerouslySetInnerHTML={{ __html: article.content }} />
-                    </article>
-                ))}
-            </div>
+            <SpacepediaNavigation/>
+
+            <Collapse
+                className={styles.content}
+                ghost
+                items={articles.map(article => ({
+                    key: article.id,
+                    label: <div className={styles.title}>{article.title}</div>,
+                    children: <div dangerouslySetInnerHTML={{__html: article.content}}/>
+                }))}
+            />
+
         </>
     );
 }
