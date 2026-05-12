710ff6dee4b1385b9ad9ca35b210af9e6370a94e
Sayrarm <minsecond@mail.ru>
2026-05-12 23:01:29 +0300

    feat: add burger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,71 @@
 import styles from './Header.module.css'
 import {Link} from "react-router-dom";
+import {useState} from "react";
 
 
 function Header() {
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // Переключение меню
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    // Закрываем меню при смене страницы
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
 
     return (
-        <>
-            <header className={styles.header}>
-                <nav className={styles.nav}>
-                    <Link className={styles.logo} to="/">
-                        <img
-                            src="/apple_1010706.png" alt="logo"
-                            width={50}
-                            height={50}/>
-                    </Link>
-                    <div className={styles.link}>
-                        <Link className={styles.a} to="/memories">Memories</Link>
-                        <Link className={styles.a} to="/characters">Characters</Link>
-                        <Link className={styles.a} to="/battle">Battle</Link>
-                        <Link className={styles.a} to="/lore">Lore</Link>
-                    </div>
-                </nav>
-            </header>
 
-        </>
+        <header className={styles.header}>
+            <nav className={styles.nav}>
+                <Link className={styles.logo} to="/">
+                    <img
+                        src="/apple_1010706.png"
+                        alt="logo"
+                        width={50}
+                        height={50}/>
+                </Link>
+                <div className={styles.link}>
+                    <Link className={styles.a} to="/memories">Memories</Link>
+                    <Link className={styles.a} to="/characters">Characters</Link>
+                    <Link className={styles.a} to="/battle">Battle</Link>
+                    <Link className={styles.a} to="/lore">Lore</Link>
+                </div>
+
+                <div className={styles.burgerContainer}>
+                    <label className={styles.burger} htmlFor="burger">
+                        <input type="checkbox"
+                               id="burger"
+                               checked={isMenuOpen}
+                               onChange={toggleMenu}/>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </label>
+                </div>
+
+                <div className={`${styles.modalNav} ${isMenuOpen ? styles.active : ''}`}>
+                    <div className={styles.border}></div>
+                    <Link className={styles.a} to="/memories" onClick={closeMenu}>
+                        Memories
+                    </Link>
+                    <Link className={styles.a} to="/characters" onClick={closeMenu}>
+                        Characters
+                    </Link>
+                    <Link className={styles.a} to="/battle" onClick={closeMenu}>
+                        Battle
+                    </Link>
+                    <Link className={styles.a} to="/lore" onClick={closeMenu}>
+                        Lore
+                    </Link>
+                </div>
+            </nav>
+        </header>
+
+
     )
 }
 
