8ee328e59e2946d96e10f631da207aae75a9f6dd
Sayrarm <minsecond@mail.ru>
2026-05-13 13:15:20 +0300

    feat: add new cards to memories data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,82 @@
-
-
 function Home() {
 
 
     return (
         <>
-            <div>Главная</div>
+            <div>
+                <h2>Banners</h2>
+                <div>
+                    <img src="" alt=""/>
+                    <h3>Title</h3>
+                    <div>Timer</div>
+                    <div>Memories</div>
+                </div>
+            </div>
+
+            <div>
+                <h2>Events</h2>
+                <div>
+                    <img src="" alt=""/>
+                    <h3>Title</h3>
+                    <div>Timer</div>
+                </div>
+            </div>
+
+            <div>
+                <h2>Promise</h2>
+                <div>
+                    <img src="" alt=""/>
+                    <div>Timer</div>
+                </div>
+            </div>
+
+            <div>
+                <h2>Reset</h2>
+
+                <div>
+
+                    <div>
+                        <h3>Daily</h3>
+                        <div>Awards</div>
+                        <div>Timer</div>
+                    </div>
+
+                    <div>
+                        <h3>Weekly</h3>
+                        <div>Awards</div>
+                        <div>Timer</div>
+                    </div>
+
+                    <div>
+                        <h3>Monthly</h3>
+                        <div>
+                            <img src="" alt=""/>
+                            Memories
+                        </div>
+                        <div>Timer</div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div>
+                <h2>Schedule</h2>
+
+                <div>
+
+                    <div>
+                        <h3>Protocores</h3>
+                        <div>protocore list</div>
+                    </div>
+
+                    <div>
+                        <h3>Deepspace Trial</h3>
+                        <div>trial list</div>
+                    </div>
+
+                </div>
+            </div>
         </>
     )
 }
