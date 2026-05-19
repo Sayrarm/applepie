49dd1afba762ee27ce8027995f5f1f4c3290c9a0
Sayrarm <minsecond@mail.ru>
2026-05-19 20:03:31 +0300

    feat: change design of Lore page

--- src/pages/Lore.jsx
+++ src/pages/Lore.jsx
@@ -9,21 +9,21 @@ function Lore() {
         <>
 
             <nav className={styles.nav}>
+                <Link className={styles.link} to="/main-story">
+                    <div className={styles.title}>Main Story</div>
+                    <img className={styles.img} src="src/assets/bg/b2_mainline_chapterbg_08.png" alt="spacepedia"/>
+                </Link>
                 <Link className={styles.link} to="/spacepedia">
-                    <img className={styles.img} src="src/assets/bg/b2_mainline_chapterbg_18.png" alt="spacepedia"/>
                     <div className={styles.title}>Spacepedia</div>
+                    <img className={styles.img} src="src/assets/bg/b2_mainline_chapterbg_18.png" alt="spacepedia"/>
                 </Link>
                 <Link className={styles.link} to="/world-underneath">
-                    <img className={styles.img} src="src/assets/bg/b2_mainline_chapterbg_09.png" alt="spacepedia"/>
                     <div className={styles.title}>World Underneath</div>
-                </Link>
-                <Link className={styles.link} to="/main-story">
-                    <img className={styles.img} src="src/assets/bg/b2_mainline_chapterbg_08.png" alt="spacepedia"/>
-                    <div className={styles.title}>Main Story</div>
+                    <img className={styles.img} src="src/assets/bg/b2_mainline_chapterbg_09.png" alt="spacepedia"/>
                 </Link>
                 <Link className={styles.link} to="/anecdotes">
-                    <img className={styles.img} src="src/assets/bg/x3_radioplay_bg.png" alt="spacepedia"/>
                     <div className={styles.title}>Anecdotes</div>
+                    <img className={styles.img} src="src/assets/bg/x3_radioplay_bg.png" alt="spacepedia"/>
                 </Link>
             </nav>
 
