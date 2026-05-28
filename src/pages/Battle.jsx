54589b56c78372550ca26268ebec6ad6ef1908d8
Sayrarm <minsecond@mail.ru>
2026-05-28 18:46:58 +0300

    feat: change Battle page

--- src/pages/Battle.jsx
+++ src/pages/Battle.jsx
@@ -1,23 +1,25 @@
-import {Collapse} from "antd";
 import styles from "./Battle.module.css";
+import {compData} from "../data/comp-data.js";
+import {Link} from "react-router-dom";
 
 function Battle() {
 
-
-
     return (
-        <>
-            <section>
-                <nav>
-                    <h1 className={styles.title}>Companions and MC Weapons</h1>
 
-                </nav>
+        <nav className={styles.nav}>
+            {compData.map(category => (
+                <Link
+                    key={category.id}
+                    className={styles.link}
+                    to={`/battle/${category.link}`}
+                >
+                    <img className={styles.img} src={category.img || category.imgWeapon} alt="image"/>
+                    <div className={styles.title}>{category.companionName || category.weaponName}</div>
+                </Link>
+            ))}
+        </nav>
 
-                <section>
-                </section>
-            </section>
-        </>
-    )
+    );
 }
 
 export default Battle
