86636f5cc35661c73e4f568ab951212840a066d2
Sayrarm <minsecond@mail.ru>
2026-06-16 20:36:29 +0300

    feat: change style for mobile version of navigation buttons

--- src/pages/CardArticle.jsx
+++ src/pages/CardArticle.jsx
@@ -10,6 +10,23 @@ import {bannersDataFull} from '../data/banners-data-full.js';
 import ObtainInfo from "../components/ObtainInfo.jsx";
 import {obtainData} from '../data/obtain-data.js';
 
+//меняем цвет svg картинке
+const IconLeft = () => (
+    <svg className={styles.iconLeft} viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M14.2322 23.2322C13.2559 24.2085 13.2559 25.7915 14.2322 26.7678L30.1421 42.6777C31.1184 43.654 32.7014 43.654 33.6777 42.6777C34.654 41.7014 34.654 40.1184 33.6777 39.1421L19.5355 25L33.6777 10.8579C34.654 9.88155 34.654 8.29864 33.6777 7.32233C32.7014 6.34602 31.1184 6.34602 30.1421 7.32233L14.2322 23.2322ZM17 25V22.5H16V25V27.5H17V25Z"
+            fill="currentColor"/>
+    </svg>
+);
+
+const IconRight = () => (
+    <svg className={styles.iconRight} viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M35.7678 26.7678C36.7441 25.7915 36.7441 24.2085 35.7678 23.2322L19.8579 7.32233C18.8816 6.34602 17.2986 6.34602 16.3223 7.32233C15.346 8.29864 15.346 9.88155 16.3223 10.8579L30.4645 25L16.3223 39.1421C15.346 40.1184 15.346 41.7014 16.3223 42.6777C17.2986 43.654 18.8816 43.654 19.8579 42.6777L35.7678 26.7678ZM32 25V27.5H34V25V22.5H32V25Z"
+            fill="currentColor"/>
+    </svg>
+);
+
 function CardArticle() {
     const {cardId} = useParams();
     const location = useLocation();
@@ -93,32 +110,34 @@ function CardArticle() {
 
                     <ParametersBlock card={card}/>
 
-                    <ObtainInfo cardId={cardId} obtainData={obtainData} />
+                    <ObtainInfo cardId={cardId} obtainData={obtainData}/>
 
-                    <BannerPeriod banners={banners} />
+                    <BannerPeriod banners={banners}/>
                 </div>
             </article>
 
             {/* Кнопки навигации */}
-            <div className={styles.navigation}>
-                <button
-                    onClick={goToPrevious}
-                    disabled={currentIndex === 0 || cardsList.length === 0}
-                    className={styles.navButton}
-                >
-                    <img className={styles.arrowsButton} src={getImageUrl('../assets/icons/left-arrow.svg')} alt="back"/>
-                </button>
-                <span className={styles.navCounter}>
-                    {cardsList.length > 0 && `${currentIndex + 1} / ${cardsList.length}`}
-                </span>
-                <button
-                    onClick={goToNext}
-                    disabled={currentIndex === cardsList.length - 1 || cardsList.length === 0}
-                    className={styles.navButton}
-                >
-                    <img className={styles.arrowsButton} src={getImageUrl('../assets/icons/right-arrow.svg')} alt="next"/>
-                </button>
-            </div>
+            {cardsList.length > 0 && (
+                <div className={styles.navigation}>
+                    <button
+                        onClick={goToPrevious}
+                        disabled={currentIndex === 0}
+                        className={styles.navButtonLeft}
+                    >
+                        <IconLeft className={styles.iconLeft}/>
+                    </button>
+                    <span className={styles.navCounter}>
+                        {currentIndex + 1} / {cardsList.length}
+                    </span>
+                    <button
+                        onClick={goToNext}
+                        disabled={currentIndex === cardsList.length - 1}
+                        className={styles.navButtonRight}
+                    >
+                        <IconRight className={styles.iconRight}/>
+                    </button>
+                </div>
+            )}
         </>
     );
 }
