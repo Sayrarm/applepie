94cc6b5e87c0cdb9e644cb97354fc56f0e850e3b
Sayrarm <minsecond@mail.ru>
2026-07-10 22:53:39 +0300

    fix: change banners data

--- src/data/banners-data-full.js
+++ src/data/banners-data-full.js
@@ -896,7 +896,7 @@ export const bannersDataFull = [
     "image": "../assets/banners/wander-in-wonder-rerun.png",
     "cardIds": [370, 178, 273, 84],
     "startDate": "2026-07-09T05:00:00",
-    "endDate": "2026-07-16T04:59:00",
+    "endDate": "2026-07-17T04:59:00",
     "rerun": true,
     "active": true
 
@@ -907,7 +907,7 @@ export const bannersDataFull = [
     "image": "../assets/banners/radiant-fantasia.png",
     "cardIds": [32, 31, 98, 99, 195, 196, 289, 290, 385, 386, 46, 45, 44, 169, 170, 171, 263, 264, 267, 358, 359, 361, 79, 80, 82],
     "startDate": "2026-07-09T05:00:00",
-    "endDate": "2026-07-23T04:59:00",
+    "endDate": "2026-07-24T04:59:00",
     "rerun": false,
     "active": true
 
