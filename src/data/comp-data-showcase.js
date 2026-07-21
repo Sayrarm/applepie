7f3ed594fe13a0a88536e2b51efb0e5a97ea0cee
Sayrarm <minsecond@mail.ru>
2026-07-21 20:06:56 +0300

    feat: add data

--- src/data/comp-data-showcase.js
+++ src/data/comp-data-showcase.js
@@ -78,62 +78,6 @@ export const compDataShowcaseSpecific = [
             hp: 25,
             def: 0
         },
-        basicAttackFormula: "",
-        basicFirstStrike: "98 + 52% ATK + 4.7% of Max HP",
-        basicFirstStrikeStats: {
-            base: 98,
-            atk: 52,
-            hp: 4.7,
-            def: 0
-        },
-        basicSecondStrike: "88 + 46% ATK + 4.2% of Max HP",
-        basicSecondStrikeStats: {
-            base: 88,
-            atk: 46,
-            hp: 4.2,
-            def: 0
-        },
-        basicThirdStrike: "122 + 65% ATK + 5.9% of Max HP",
-        basicThirdStrikeStats: {
-            base: 122,
-            atk: 65,
-            hp: 5.9,
-            def: 0
-        },
-        basicFourthStrike: "147 + 78% ATK + 7.1% of Max HP",
-        basicFourthStrikeStats: {
-            base: 147,
-            atk: 78,
-            hp: 7.1,
-            def: 0
-        },
-        basicChargedAttack: "165 + 88% ATK + 7.9% of Max HP",
-        basicChargedAttackStats: {
-            base: 165,
-            atk: 88,
-            hp: 7.9,
-            def: 0
-        },
-        activeSkillFormula: "351 + 187% ATK + 16.8% of Max HP",
-        activeSkillStats: {
-            base: 351,
-            atk: 187,
-            hp: 16.8,
-            def: 0
-        },
-        activeSkillSecondFormula: "376 + 200% ATK + 18% of Max HP",
-        activeSkillSecondStats: {
-            base: 376,
-            atk: 200,
-            hp: 18,
-            def: 0
-        },
-        buffPassiveSkillFormula: `Under Darknight Reign 
-+ 8% damage dealt + 8% DMG Boost to Weakened`,
-        buffPassiveSkillStats: {
-            DMGBoost: 8,
-            WeakenedBoost: 8,
-        },
         buffEidolon0Formula: "",
         buffEidolon1Formula: "+ 40% Active Skill damage under Darknight Reign",
         buffEidolon1Stats: {
@@ -192,68 +136,6 @@ export const compDataShowcaseSpecific = [
             atk: 0,
             hp: 0,
             def: 0
-        },
-        basicAttackFormula: "",
-        basicFirstStrike: "55 + 29% ATK + 117% DEF",
-        basicFirstStrikeStats: {
-            base: 55 ,
-            atk: 29,
-            hp: 0,
-            def: 117
-        },
-        basicSecondStrike: "50 + 26% ATK + 105% DEF",
-        basicSecondStrikeStats: {
-            base: 50,
-            atk: 26,
-            hp: 0,
-            def: 105
-        },
-        basicThirdStrike: "66 + 35% ATK + 140% DEF",
-        basicThirdStrikeStats: {
-            base: 66,
-            atk: 35,
-            hp: 0,
-            def: 140
-        },
-        basicFourthStrike: "72 + 38% ATK + 152% DEF",
-        basicFourthStrikeStats: {
-            base: 72,
-            atk: 38,
-            hp: 0,
-            def: 152
-        },
-        basicFifthStrike: "77 + 41% ATK + 163% DEF",
-        basicFifthStrikeStats: {
-            base: 77,
-            atk: 41,
-            hp: 0,
-            def: 163
-        },
-        basicChargedAttack: "150 + 80% ATK + 317% DEF",
-        basicChargedAttackStats: {
-            base: 150,
-            atk: 80,
-            hp: 0,
-            def: 317
-        },
-        activeSkillFormula: "403 + 215% ATK + 852% DEF",
-        activeSkillStats: {
-            base: 403,
-            atk: 215,
-            hp: 0,
-            def: 852
-        },
-        activeSkillSecondFormula: "",
-        activeSkillSecondStats: {
-            base: 0,
-            atk: 0,
-            hp: 0,
-            def: 0
-        },
-        buffPassiveSkillFormula: ``,
-        buffPassiveSkillStats: {
-            DMGBoost: 0,
-            WeakenedBoost: 0,
         },
         buffEidolon0Formula: "",
         buffEidolon1Formula: "+ 25% DMG and for Moonlight",
@@ -330,6 +212,71 @@ export const weaponDataShowcaseSpecific = [
         buffPassiveSkillStats: {
             DMGBoost: 8,
             WeakenedBoost: 8,
+        },
+    },
+    {
+        weaponName: "Moonchaser",
+        basicAttackFormula: "",
+        basicFirstStrike: "55 + 29% ATK + 117% DEF",
+        basicFirstStrikeStats: {
+            base: 55 ,
+            atk: 29,
+            hp: 0,
+            def: 117
+        },
+        basicSecondStrike: "50 + 26% ATK + 105% DEF",
+        basicSecondStrikeStats: {
+            base: 50,
+            atk: 26,
+            hp: 0,
+            def: 105
+        },
+        basicThirdStrike: "66 + 35% ATK + 140% DEF",
+        basicThirdStrikeStats: {
+            base: 66,
+            atk: 35,
+            hp: 0,
+            def: 140
+        },
+        basicFourthStrike: "72 + 38% ATK + 152% DEF",
+        basicFourthStrikeStats: {
+            base: 72,
+            atk: 38,
+            hp: 0,
+            def: 152
+        },
+        basicFifthStrike: "77 + 41% ATK + 163% DEF",
+        basicFifthStrikeStats: {
+            base: 77,
+            atk: 41,
+            hp: 0,
+            def: 163
+        },
+        basicChargedAttack: "150 + 80% ATK + 317% DEF",
+        basicChargedAttackStats: {
+            base: 150,
+            atk: 80,
+            hp: 0,
+            def: 317
+        },
+        activeSkillFormula: "403 + 215% ATK + 852% DEF",
+        activeSkillStats: {
+            base: 403,
+            atk: 215,
+            hp: 0,
+            def: 852
+        },
+        activeSkillSecondFormula: "",
+        activeSkillSecondStats: {
+            base: 0,
+            atk: 0,
+            hp: 0,
+            def: 0
+        },
+        buffPassiveSkillFormula: ``,
+        buffPassiveSkillStats: {
+            DMGBoost: 0,
+            WeakenedBoost: 0,
         },
     },
 ]
