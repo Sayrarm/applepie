1d17742b649b3d8bf5175c64f9774894736a5653
Sayrarm <minsecond@mail.ru>
2026-07-26 02:32:27 +0300

    feat: add data; change styles

---
+++ src/data/about-data.js
@@ -0,0 +1,66 @@
+export const aboutData = [
+    {
+        name: "about",
+        text: `Hello there!
+        This project was created to help you plan and manage your resources in the game “Love and Deepspace”. The website is a set of small features that are created to make life easier for both new players and those who have been playing for a long time but are stuck in the endgame content:`,
+    },
+    {
+        name: "point_1.1",
+        text: `• My resources: add Bottles of Wishes and Core Energy to see how much EXP you have to level up your Memories and Protocores; adding Memory Heartsand and Crystal Box shows you what resources you can replace them with.`,
+        img: "",
+    },
+    {
+        name: "point_1.2",
+        text: `• Protocore Calculator and Memory Calculator: you can quickly check how many resources and stamina you will need to lvl up your Protocores/Memories.`,
+        img: "",
+    },
+    {
+        name: "point_1.3",
+        text: `• Farm Goal Tracker: The main page displays information about the collection of resources for the farm goal (Protocores and Memories). Update My Resources to keep track of how many resources you have left for farming.`,
+        img: "",
+    },
+    {
+        name: "point_1.4",
+        text: `• My Protocores, My Memories: add your Protocores and Memories to build a team in Showcase:`,
+        img: "",
+    },
+    {
+        name: "item_1",
+        text: `1) My Protocores – add protocores that can be placed on your Memories;`,
+        img: "",
+    },
+    {
+        name: "item_2",
+        text: `2) My Memories – this is where all your Memories are listed with all their stats.`,
+        img: "",
+    },
+    {
+        name: "item_3",
+        text: `3) Showcase – choose a Companion, MC Weapon, Solar and Lunar Memories to build your team. Below is an approximate calculation of the damage you will receive in the game.`,
+        img: "",
+    },
+    {
+        name: "about_2",
+        text: `The website also provides information about:`,
+    },
+    {
+        name: "point_2.1",
+        text: `• Banners: information about all the banners that have been featured in the game.`,
+        img: "",
+    },
+    {
+        name: "point_2.2",
+        text: `• Lore: At the moment, all the text from Spacepedia, World Underneath, and Anecdotes is presented.`,
+        img: "",
+    },
+    {
+        name: "point_2.3",
+        text: `• Memories: All the Memories from the game are presented here. You can click on any Memory and view information about it: where the Memory can be obtained and from which banners, and the Memory's characteristics. You can mark a Memory as available to indicate its level, rank, and Memory protocores.`,
+        img: "",
+    },
+    {
+        name: "point_2.4",
+        text: `• Battle: This section provides a description of all the existing Companions and MC Weapons in the game.`,
+        img: "",
+    },
+]
