edb0cf30b084a6668921b681df5b894f9be2064f
Sayrarm <minsecond@mail.ru>
2026-08-02 04:09:41 +0300

    feat: add disclaimer for Character Article

--- src/pages/CharacterArticle.jsx
+++ src/pages/CharacterArticle.jsx
@@ -1,8 +1,9 @@
 import {Link, useParams} from "react-router-dom";
-import {Fragment} from "react";
-import styles from "./Article.module.css";
+import React, {Fragment} from "react";
+import styles from "./CharacterArticle.module.css";
 import {charactersCategories} from "../data/characters-categories.js";
 import CharacterArticlePage from "../components/CharacterArticlePage.jsx";
+import AsideList from "../components/AsideList.jsx";
 
 
 
@@ -12,6 +13,44 @@ function CharacterArticle() {
 
     // Находим текущую категорию
     const currentCategory = charactersCategories.find(cat => cat.link === articleLink);
+
+    const items = [
+        {
+            key: '1',
+            label: 'Spoiler Alert',
+            children: (
+                <div className={styles.disclaimerContent}>
+                    <strong className={styles.titleMain}>
+                        This page provides information about the character's story.  If you don't want spoilers, please open the links with caution :)
+                    </strong>
+                    <p>
+                        <strong>Main Story</strong> - the main storyline. Below are the chapters in which the character appears (both in full and cameo). Presented as a visual novel + animation;
+                    </p>
+                    <p>
+                        <strong>Bond</strong> - a story that opens when the Affinity level increases. Presented as an animation;
+                    </p>
+                    <p>
+                        <strong>World Underneath</strong> - a story that opens when you open Main Story. Presented as text.
+                    </p>
+                    <p>
+                        <strong>Anecdotes</strong> - a story that opens when you receive multiple Character Memories or open Main Story. Presented as text.
+                    </p>
+                    <p>
+                        <strong>Myths</strong> - a major plot that is presented in several chapters (usually 9). Specific Memories are required. Presented as a visual novel + animation;
+                    </p>
+                    <p>
+                        <strong>Memoria</strong> - story/dates that can be obtained from 5-star Memories (and some 4-star Memories). Presented as a visual novel + animation;
+                    </p>
+                    <p>
+                        <strong>Tender Moments</strong> - a story that can be obtained from 4-star Memories. Presented as an audiobook (MC is not voiced, subtitles must be read).
+                    </p>
+                    <p>
+                        <strong>Secret Times</strong> - a story that can be obtained from 4-star Memories or reaching the required Affinity level. Presented as an audio track with only the male character voiced.
+                    </p>
+                </div>
+            ),
+        },
+    ];
 
     return (
         <>
@@ -29,6 +68,11 @@ function CharacterArticle() {
                     )}
                 </Fragment>
             </nav>
+
+            <div className={styles.titleAndDisclaimer}>
+                <AsideList
+                    items={items}/>
+            </div>
 
             <CharacterArticlePage />
         </>
