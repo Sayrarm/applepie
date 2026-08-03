9f4b3356f4702271cedfbda7285acf171358117e
Sayrarm <minsecond@mail.ru>
2026-08-03 18:46:44 +0300

    fix: change cards info for new banner

--- src/data/banners-data-full.js
+++ src/data/banners-data-full.js
@@ -960,7 +960,7 @@ export const bannersDataFull = [
     "id": 88,
     "name": "Summer Surge",
     "image": "../assets/banners/summer-surge.jpg",
-    "cardIds": [32, 31, 98, 99, 195, 196, 289, 290, 385, 386, 46, 45, 44, 169, 170, 171, 263, 264, 267, 358, 359, 361, 79, 80, 82],
+    "cardIds": [],
     "startDate": "2026-08-05T05:00:00",
     "endDate": "2026-08-15T04:59:59",
     "rerun": false,
