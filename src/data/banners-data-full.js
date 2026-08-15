4be6bd9304ed7d4431bb408b5bb1ef857cee692a
Sayrarm <minsecond@mail.ru>
2026-08-15 20:12:07 +0300

    feat: add banners info

--- src/data/banners-data-full.js
+++ src/data/banners-data-full.js
@@ -964,6 +964,17 @@ export const bannersDataFull = [
     "startDate": "2026-08-05T05:00:00",
     "endDate": "2026-08-15T04:59:59",
     "rerun": false,
+    "active": false
+
+  },
+  {
+    "id": 89,
+    "name": "Martial Ode",
+    "image": "../assets/banners/Martial-Ode.jpg",
+    "cardIds": [396],
+    "startDate": "2026-08-17T05:00:00",
+    "endDate": "2026-08-31T04:59:59",
+    "rerun": false,
     "active": true
 
   },
